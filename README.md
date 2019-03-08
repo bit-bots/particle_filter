4ee5e5bbd87797c2a798b53184458ed8c21fd7bf
Niklas Fiedler <git@nfiedler.de>
2019-03-08 16:01:34 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # particle_filter
+
 A particle filter library for the ROS context.
+
+This library is based on libPF by Stephan Wirth.
---
+++ include/particle_filter/Clusterizer.h
@@ -0,0 +1,16 @@
+//
+// Created by nfiedler on 11/5/18.
+//
+
+#ifndef BITBOTS_WORLD_MODEL_CLUSTERIZER_H
+#define BITBOTS_WORLD_MODEL_CLUSTERIZER_H
+
+
+template <typename StateType>
+typedef struct GMM {
+    StateType mean
+};
+
+
+
+#endif //BITBOTS_WORLD_MODEL_CLUSTERIZER_H
